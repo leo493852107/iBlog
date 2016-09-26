1957342525263b0320505981b0cdcfa5a9ad451a
leo <leo493852107@163.com>
2016-09-26 08:06:04 +0800

    添加所有依赖 && add some toll

---
+++ app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by leo on 25/09/2016.
+ */
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var route = require('')
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by leo on 25/09/2016.
+ */
+
+var express = require('express');
+var route = express.Router();
+var path = require('path');
+var async = require('async');
+
+
