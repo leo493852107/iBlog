6766061c89038c5f02154a8cef9fa693dfdf0e93
leo <leo493852107@163.com>
2016-09-26 20:00:11 +0800

    参考 /login

--- app.js
+++ app.js
@@ -7,6 +7,65 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var bodyParser = require('body-parser');
+var passport = require('passport');
 
-var route = require('')
+var route = require('./routes/index');
+var auth = require('./routes/auth');
+var logger = require('./utility/logger');
+var i18n = require('./models/i18n');
+
+var app = express();
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+// i18n init parses req for language headers, cookies, etc.
+app.use(i18n.init);
+
+app.use(session({
+    secret: 'iblog-exp-session',
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000
+    },
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//app.use('/', route);
+
+app.use('/', auth);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error();
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+app.use(function (err, req, res, next) {
+    var code = err.status || 500,
+        message = code === 404 ? res.__('error.404_1') : res.__('error.404_2');
+    res.status(code);
+    logger.errLogger(req, err);
+    res.render('./shared/error', {
+        code: code,
+        message: message
+    });
+});
+
+module.exports = app;
